13533238dfe763131016542b46973021db624bc1
Shakeeb <shakeebparavannur@gmail.com>
2023-10-10 00:37:09 +0530

    add:axios for api call\

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import {MatTableModule } from '@angular/material/table'
+import { MatTableModule } from '@angular/material/table';
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products/products.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import axios from 'axios';
 
 @NgModule({
-  declarations: [ // Every angular component that exist in the angular module must be added to the decalaration array
-    AppComponent, ProductsComponent
+  declarations: [
+    // Every angular component that exist in the angular module must be added to the decalaration array
+    AppComponent,
+    ProductsComponent,
   ],
-  imports: [ // Defines other Angular modules that contain the functionality the Angular module needs.
-    BrowserModule,HttpClientModule, BrowserAnimationsModule,MatTableModule
+  imports: [
+    // Defines other Angular modules that contain the functionality the Angular module needs.
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    
+    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import axios from 'axios';
 
 @Component({
   selector: 'app-products',
@@ -12,13 +13,17 @@ export class ProductsComponent implements OnInit {
   tableHeaders:string[] =['id','title','price','brand']
 
   constructor(private http: HttpClient) {}
-  ngOnInit(): void {
+ async ngOnInit () {
     const apiUrl = `https://dummyjson.com/products`;
-    this.http.get(apiUrl).subscribe((data) => {
-      this.apiData = data;
-      console.log(data);
-      this.products = this.apiData.products      ;
-      console.log(data, 'sadasdasd');
-    });
+ 
+    const prod= await axios.get(apiUrl);
+    console.log(prod.data.products);
+    this.products = prod.data.products;
   }
 }
+   // this.http.get(apiUrl).subscribe((data) => {
+    //   this.apiData = data;
+    //   console.log(data);
+    //   this.products = this.apiData.products      ;
+    //   console.log(data, 'sadasdasd');
+    // });
